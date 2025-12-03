0f2350597031c1d7bfe5cfc98730f807eab39ed3
Rodrigo Dias <screaming.andmore@gmail.com>
2025-12-03 16:26:42 -0300

    SERVICE WORK

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -11,12 +12,12 @@ export default defineConfig(({ mode }) => {
       react(),
       VitePWA({
         registerType: 'autoUpdate',
-        injectRegister: 'auto', // LIGA O PILOTO AUTOMÁTICO
+        injectRegister: 'auto', // Reativado para gerenciar o registro automaticamente e evitar erros de path/origem
         strategies: 'generateSW',
-        filename: 'chef-sw.js', // Nome do arquivo gerado
-        manifest: false, // Respeita o seu arquivo manual public/manifest.json
+        filename: 'chef-sw.js', // Nome fixo do arquivo
+        manifest: false, // Respeita o arquivo manual public/manifest.json
         devOptions: {
-          enabled: true, // Funciona no localhost
+          enabled: true,
           type: 'module',
         },
         includeAssets: [
